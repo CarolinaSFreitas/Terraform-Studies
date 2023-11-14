993331cd2c1c2b345ce92781b9b5b996372de025
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2023-11-14 17:23:31 -0300

    Add files via upload

--- 2º TerraformTuto - KodeKloud/local.tf
+++ 2º TerraformTuto - KodeKloud/local.tf
@@ -1,5 +1,10 @@
 resource "local_file" "pet" {
-    filename = "root/pets.txt"
+    filename = "carol/pets.txt"
     content = "Théo Meu Pet Favorito"
     file_permission = "0700"
 }
+
+resource "local_file" "theo" {
+    filename = "carol/theo.txt"
+    content = "Théo é o nome do meu cachorro :)"
+    file_permission = "0700"

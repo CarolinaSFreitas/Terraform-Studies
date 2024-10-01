56f334d43a35246adef9e34ed0fd8d9b589cf5b3
CarolinaSFreitas <carolsoaresfreitas@hotmail.com>
2024-10-01 15:44:20 -0300

    feat: Adicionar arquivos e diretórios do Terraform e atualizar arquivo .gitignore

---
+++ 6º Terraform - Do Básico do Avançado/show-state-import-refresh/bucket.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "bucket_1" {
+  bucket = "carolf-comandos-terraform-1"
+}
+
+resource "aws_s3_bucket" "bucket_2" {
+  bucket = "carolf-comandos-terraform-2"
+}
+
+resource "aws_s3_bucket" "bucket_3" {
+  bucket = "carolf-comandos-terraform-3"
+}

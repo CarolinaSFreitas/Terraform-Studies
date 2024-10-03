0a974e6cc142b8abc92134b11f49ef26a7988d64
CarolinaSFreitas <carolsoaresfreitas@hotmail.com>
2024-10-03 14:33:01 -0300

    Provisioners no Azure

---
+++ 6º Terraform - Do Básico do Avançado/azure-vm-provisioners/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  common_tags = {
+    Owner      = "Carol"
+    managed_by = "Terraform"
+  }
+}
---
+++ 6º Terraform - Do Básico do Avançado/azure-vm-provisioners/variables.tf
@@ -0,0 +1,6 @@
+variable "location" {
+  description = "Região onde a VM será criada"
+  type        = string
+  default     = "East US"
+}
+

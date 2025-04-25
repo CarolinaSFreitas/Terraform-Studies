3dcabf5d5e4eeef8ed528accea775ec10fa66a4c
CarolinaSFreitas <carolsoaresfreitas@hotmail.com>
2025-04-25 20:49:26 -0300

    Adicionar arquivos de configuração do Terraform: .terraform.lock.hcl, main.tf e provider.tf

---
+++ 7º Descomplicando o Terraform/aula_comandos_basicos_backend/main.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_subnet" "default" {
+  vpc_id                  = "vpc-0cd134e0fa01daada" 
+  cidr_block              = "172.31.1.0/24" 
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Default Subnet"
+  }
+}
